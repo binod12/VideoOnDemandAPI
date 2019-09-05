0ab79cb5c8e59d5df44a1c0e481fa4fb1f40743e
Binod Singh <binod12@gmail.com>
2019-09-05 14:46:47 +0530

    Revert "Revert "Initial Framework ready - Refactoring remaining""
    This reverts commit e2cd9057a7d624cc052372793b48d5e4daf6b826.

---
+++ VideoOnDemandAPI/Models/DataGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoOnDemandAPI.Models
+{
+  public class DataGenerator
+  {
+
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+      using (var context = new VideoOnDemandDBContext(
+          serviceProvider.GetRequiredService<DbContextOptions<VideoOnDemandDBContext>>()))
+      {
+        // Look for any board games already in database.
+        if (context.Users.Any())
+        {
+          return;   // Database has been seeded
+        }
+
+        context.Users.AddRange(
+         new User { Id = 1, FirstName = "Admin", LastName = "User", Username = "admin", Password = "admin", Role = Role.Admin },
+         new User { Id = 2, FirstName = "Normal", LastName = "User", Username = "user", Password = "user", Role = Role.User });
+
+        context.SaveChanges();
+      }
+    }
+  }
+}
---
+++ VideoOnDemandAPI/Models/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoOnDemandAPI.Models
+{
+  public class Video: EntityBase
+  {
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public VideoCategory Category { get; set; }
+
+    public double Price { get; set; }
+  }
+}
